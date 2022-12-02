10a635a81e301f957cc5a166793eb8fd38536b2a
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2022-12-02 11:05:26 +0100

    serviceWorker.js funktioniert

--- stellar-spectrum/public/serviceWorker.js
+++ stellar-spectrum/public/serviceWorker.js
@@ -1,124 +1,124 @@
 // Globe Variablen
- const assets = [
-    "/",
-    "/instrumentenkauf/",
-    "/honorar/",
-    "/leistungen/",
-    "/faq/",
-    "/download/",
-    "/impressum/",
-    "/klavierunterricht/",
-    "/keyboardunterricht/",
-    "/gitarrenunterricht/",
-    "/e-bassunterricht/",
-    "/e-gitarrenunterricht/",
-    "/banjounterricht/",
-    "/akkordeon-unterricht/",
-    "/saxophonunterricht/",
-    "/klarinettenunterricht/",
-    "/noten_lesen/",
-    "/intervalle/",
-    "/triads/",
-    "/functional_theory/"
-]; 
+const assets = [
+  "/",
+  "/instrumentenkauf/",
+  "/honorar/",
+  "/leistungen/",
+  "/faq/",
+  "/download/",
+  "/impressum/",
+  "/klavierunterricht/",
+  "/keyboardunterricht/",
+  "/gitarrenunterricht/",
+  "/e-bassunterricht/",
+  "/e-gitarrenunterricht/",
+  "/banjounterricht/",
+  "/akkordeon-unterricht/",
+  "/saxophonunterricht/",
+  "/klarinettenunterricht/",
+  "/noten_lesen/",
+  "/intervalle/",
+  "/triads/",
+  "/functional_theory/",
+];
 const cacheTypes = ["main", "fonts", "image"];
 const cacheVersion = "_v3";
 const cacheKey = "MyFancyCacheName_v1";
 
-
 self.addEventListener("install", (event) => {
-    // waitUntil - hält den SW in installing status etwas zu machen bevor Event abgeschlossen wird
-    event.waitUntil(
-        // Zugriff auf die Cache API im Browser um komplette Requests und Response zu speichern
-        caches.open(cacheTypes[0] + cacheVersion + cacheKey).then((cache) =>
-            {
-                //Fügt alle Assets zum cache hinzu
-                return cache.addAll(assets);
-            })
-        .then(self.skipWaiting())
-    );
+  // waitUntil - hält den SW in installing status etwas zu machen bevor Event abgeschlossen wird
+  event.waitUntil(
+    // Zugriff auf die Cache API im Browser um komplette Requests und Response zu speichern
+    caches
+      .open(cacheTypes[0] + cacheVersion + cacheKey)
+      .then((cache) => {
+        //Fügt alle Assets zum cache hinzu
+        return cache.addAll(assets);
+      })
+      .then(self.skipWaiting())
+  );
 });
 
 function putInCache(request, response) {
-    //Console.log(request, response);
+  //Console.log(request, response);
 
-    let cacheKey = cacheTypes.includes(request.destination)
+  let cacheKey = cacheTypes.includes(request.destination)
     ? request.destination
     : "main";
-    caches.open(cacheKey + cacheVersion).then((cache) => {
-        cache.put(request, response);
-    })
+  caches.open(cacheKey + cacheVersion).then((cache) => {
+    cache.put(request, response);
+  });
 }
 
 async function cacheFirst(request) {
-    let responseFromCache = await caches.match(request)
-    if (responseFromCache) {
-        return responseFromCache;
-    }
+  let responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
 
-    let responseFrameNetwork = await fetch(request);
-    putInCache(request, responseFrameNetwork.clone());
-    return responseFrameNetwork;
+  let responseFrameNetwork = await fetch(request);
+  putInCache(request, responseFrameNetwork.clone());
+  return responseFrameNetwork;
 }
 
 async function networkFirst(request) {
-    try {
-        const responseFrameNetwork = await fetch(request);
-        putInCache(request, responseFrameNetwork.clone());
-        return responseFrameNetwork;
-    }   catch {
-        const responseFromCache = await caches.match(request);
-        if (responseFromCache) {
-            return responseFromCache;
-        }
+  try {
+    const responseFrameNetwork = await fetch(request);
+    putInCache(request, responseFrameNetwork.clone());
+    return responseFrameNetwork;
+  } catch {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
     }
+  }
 }
 
 self.addEventListener("fetch", (event) => {
-    //console.log (event.request);
-    let response = "";
-        switch (event.request.destination) {
-            case "font":
-                response = cacheFirst(event.request);
-            break;
-        case "image":
-            response = cacheFirst(event.request);
-            break;
-            default:
-                response = networkFirst(event.request);
-    }
-    event.respond.With(response);
+  //console.log (event.request);
+  let response = "";
+  switch (event.request.destination) {
+    case "font":
+      response = cacheFirst(event.request);
+      break;
+    case "image":
+      response = cacheFirst(event.request);
+      break;
+    default:
+      response = networkFirst(event.request);
+  }
+  event.respond.With(response);
 });
 
 // Aktivate SW
 async function deleteOldCache() {
-    // Welche Caches sollen erhalten bleiben
-    let cacheKeepList = [];
-    cacheTypes.forEach((element) => {
-        cacheKeepList.push(element + cacheVersion);
-    });
+  // Welche Caches sollen erhalten bleiben
+  let cacheKeepList = [];
+  cacheTypes.forEach((element) => {
+    cacheKeepList.push(element + cacheVersion);
+  });
 
-    // All caches werden ermittelt und gefiltert
-    let keyList = await caches.keys();
-    let cacheToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+  // All caches werden ermittelt und gefiltert
+  let keyList = await caches.keys();
+  let cacheToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
 
-    // Löschen der Caches welche nicht benötigt werden
-    return Promise.all(
-       cacheToDelete.keys.map((key) => {
-        if (!cacheAllowList.includes(key)) {
-            return caches.delete(key);
-        }
-       }) 
-    );
+  // Löschen der Caches welche nicht benötigt werden
+  return Promise.all(
+    cacheToDelete.keys.map((key) => {
+      if (!cacheAllowList.includes(key)) {
+        return caches.delete(key);
+      }
+    })
+  );
 }
 
 self.addEventListener("activate", (event) => {
-    console.log("service worker aktivated");
-    event.waitUntil(
-        deleteOldCache().then(() => {
-            //Damit alle Clients (Tabs im Browser) den aktivierten Service Worker nutzen
-            // sonst erst nach erneutem Reload der Seite
-            clients.claim();
-        })
-    );
+  console.log("service worker aktivated");
+  event.waitUntil(
+    deleteOldCache().then(() => {
+      //Damit alle Clients (Tabs im Browser) den aktivierten Service Worker nutzen
+      // sonst erst nach erneutem Reload der Seite
+      clients.claim();
+    })
+  );
 });
