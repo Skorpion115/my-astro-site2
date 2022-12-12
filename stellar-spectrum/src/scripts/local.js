1183135c02aea6257bd1176978c2080614ec756e
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2022-12-12 18:58:35 +0100

    changed

--- stellar-spectrum/src/scripts/local.js
+++ stellar-spectrum/src/scripts/local.js
@@ -1,4 +1,4 @@
-// Google Tag Manager
+// Google Tag Manager wird alles gebündelt
   (function (w, d, s, l, i) {
     w[l] = w[l] || [];
     w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
