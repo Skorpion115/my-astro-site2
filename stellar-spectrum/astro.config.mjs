7f4af8d6773e65356be32dbce121f2669b593505
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-01-10 14:46:41 +0100

    https://musicstudio-ziebart.de changed

--- stellar-spectrum/astro.config.mjs
+++ stellar-spectrum/astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
     // static oder server SSR serverseitiges rändern
     output: 'static',
     // Die endgültige Seite bei deinem Hostanbieter
-    site: 'https://musicstudio-ziebart.netlify.app',
+    site: 'https://musicstudio-ziebart.de',
     // Sitemap intergrieren, Eine Seite aufnehmen die nicht mit Astro erstellt wurde stillgelegt!!!
     /*
     integrations: [sitemap({
